de579b079860d3a9b2d44e5e20ab9abb6ef14395
Feng Zheng <zhengf@synopsys.com>
2019-06-14 10:05:14 -0400

    temp edit

--- pyEX/markets.py
+++ pyEX/markets.py
@@ -6,15 +6,15 @@ from deprecation import deprecated
 from .common import _getJson, _toDatetime
 
 
-@deprecated(details='Deprecated: IEX Cloud status unkown')
+@deprecated(details='Deprecated: IEX Cloud status unknown')
 def markets(token='', version=''):
-    '''https://iextrading.com/developer/docs/#intraday'''
+    """https://iextrading.com/developer/docs/#intraday"""
     return _getJson('market', token, version)
 
 
-@deprecated(details='Deprecated: IEX Cloud status unkown')
+@deprecated(details='Deprecated: IEX Cloud status unknown')
 def marketsDF(token='', version=''):
-    '''https://iextrading.com/developer/docs/#intraday'''
+    """https://iextrading.com/developer/docs/#intraday"""
     df = pd.DataFrame(markets(token, version))
     _toDatetime(df)
     return df
